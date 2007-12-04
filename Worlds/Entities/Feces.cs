b216c671b9ef10845a3db9fe0c6d17b616d96677
tristanlabelle <tristanlabelle@a8b767b7-1540-0410-8c98-598fa735f374>
2007-12-04 21:33:54 +0000

    Replaced Feces back by Excrement, for symmetry with name of operator Defecate.

--- Worlds/Entities/Feces.cs
+++ Worlds/Entities/Feces.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace AntiCulture.Worlds.Entities
 {
-    public class Excrement : Entity
+    public class Feces : Entity
     {
         #region Species stuff
         public new static readonly Species Species;
 
-        static Excrement()
+        static Feces()
         {
-            Species = new Species("excrement", "scrunge", Factory);
+            Species = new Species("feces", "scrunge", Factory);
             Species.Properties["weight"] = 0.25f;
             Species.Properties["nutrition"] = 0.02f;
             Species.Properties["healing"] = -0.4f;
         }
 
-        private static Entity Factory(World world) { return new Excrement(); }
+        private static Entity Factory(World world) { return new Feces(); }
         #endregion
 
-        public Excrement()
+        public Feces()
             : base(Species)
         {
             Integrity = 1.0f;
