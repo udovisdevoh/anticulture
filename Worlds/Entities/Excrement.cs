9795eafbfed26f49c7feab8dae79971acc5d9e95
tristanlabelle <tristanlabelle@a8b767b7-1540-0410-8c98-598fa735f374>
2007-12-04 21:28:57 +0000

    Renaming Feces to Excrement for understandability.

--- Worlds/Entities/Excrement.cs
+++ Worlds/Entities/Excrement.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace AntiCulture.Worlds.Entities
 {
-    public class Feces : Entity
+    public class Excrement : Entity
     {
         #region Species stuff
         public new static readonly Species Species;
 
-        static Feces()
+        static Excrement()
         {
-            Species = new Species("feces", "scrunge", Factory);
+            Species = new Species("excrement", "scrunge", Factory);
             Species.Properties["weight"] = 0.25f;
             Species.Properties["nutrition"] = 0.02f;
             Species.Properties["healing"] = -0.4f;
         }
 
-        private static Entity Factory(World world) { return new Feces(); }
+        private static Entity Factory(World world) { return new Excrement(); }
         #endregion
 
-        public Feces()
+        public Excrement()
             : base(Species)
         {
             Integrity = 1.0f;
